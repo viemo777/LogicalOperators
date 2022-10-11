2826e8cd2bfd835fcdb5041cf884bd0e45e00574
viemo777 <viemo777@gmail.com>
2022-10-11 15:57:03 +0300

    Initial commit

---
+++ logical_operators.py
@@ -0,0 +1,18 @@
+print(2 > 1 and 30 > 20)
+print(2 < 1 and 30 > 20)
+print(2 > 1 and 30 < 20)
+print(2 < 1 and 30 < 20)
+
+
+print(2 > 1 or 30 > 20)
+print(2 < 1 or 30 > 20)
+print(2 > 1 or 30 < 20)
+print(2 < 1 or 30 < 20)
+
+print(True and True)
+print(True or True)
+print(False and False)
+print(False or False)
+
+print(not True)
+print(not False)
